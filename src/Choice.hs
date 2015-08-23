793bc55186b3a1e685b259641e9b2d6d8447181b
Brian Hurt <bhurt42@gmail.com>
2015-08-23 09:47:45 -0400

    Change literate haskell to just normal haskell.

--- src/Choice.hs
+++ src/Choice.hs
@@ -1,12 +1,12 @@
 
-\chapter{Choice}
-\label{chap:Choice}
-
-\section{Module}
-
->   module Choice where
-
-\section{Choice data structure}
+{-|
+Module              : Choice
+Description         : The Choice datastructure and basic operations
+Copyright           : (c) Brian Hurt, 2015
+License             : GPL-3
+Maintainer          : bhurt42@gmail.com
+Stability           : experimental
+Portability         : safe
 
 The biggest problem in making a compiler backend is that there are three
 different algorithms: instruction selection, register allocation, and
@@ -17,21 +17,21 @@ decision for one algorithm to pick may change depending upon the state
 of the other algorithms.
 
 For example, on the Intel x86 architecture, if we want to add the
-constant {\tt 7} to an expression, we can do this in (at least) two
-different ways.  We can use the {\tt ADD} instruction, or we can use the
-{\tt LEA} (Load Effective Address) instruction.  Which of these two
+constant @7@ to an expression, we can do this in (at least) two
+different ways.  We can use the @ADD@ instruction, or we can use the
+@LEA@ (Load Effective Address) instruction.  Which of these two
 instructions should the instruction selection algorithm pick?
 
 This depends upon the results of the other two algorithms.  The target
-of the {\tt ADD} instruction can be in memory, while this is not true
-for the {\tt LEA}- however, the result of the {\tt LEA} can be stored in
-a different register than the target, which is not true of the {\tt ADD}
+of the @ADD@ instruction can be in memory, while this is not true
+for the @LEA@- however, the result of the @LEA@ can be stored in
+a different register than the target, which is not true of the @ADD@
 instruction.  So the decisions of the register allocator influence which
 of the two possible instructions is better.
 
 But the instruction scheduler also has implications for which
-instruction should be selected.  The {\tt ADD} instruction takes an
-arithmetic functional unit, while the {\tt LEA} uses a load functional
+instruction should be selected.  The @ADD@ instruction takes an
+arithmetic functional unit, while the @LEA@ uses a load functional
 unit.  Which instruction is optimal to emit depends upon what other
 instructions are being issued at the same time, and thus what functional
 units are free.
@@ -55,94 +55,90 @@ complicated algorithms that might produce better results are often not
 implemented, due simply to the complexity of doing so.  Certainly
 experimentation is reduced due to the high cognitive overhead.
 
-\subsection{Choice}
-
 BAM approaches the problem in a radically different way.  Rather than
 trying to figure out the right decision apriori, it simply records that
 a choice needs to be made.  Rather than the instructions being created
 in a simple list, they are produced in a multi-tail list, or choice
 tree.
 
->   data Choice a =
->       Choice (Choice a) (Choice a)
->       | Emit a (Choice a)
->       | Done
+-}
+module Choice where
 
->   toList :: Choice a -> [ [ a ] ]
->   toList (Choice a b) = toList a ++ toList b
->   toList (Emit x a) = fmap (x:) (toList a)
->   toList Done = [ [] ]
+    data Choice a =
+        Choice (Choice a) (Choice a)
+        | Emit a (Choice a)
+        | Done
 
-\subsection{Constructors}
+    toList :: Choice a -> [ [ a ] ]
+    toList (Choice a b) = toList a ++ toList b
+    toList (Emit x a) = fmap (x:) (toList a)
+    toList Done = [ [] ]
 
->   emit :: a -> Choice a
->   emit x = Emit x Done
+    emit :: a -> Choice a
+    emit x = Emit x Done
 
->   choice2 :: Choice a -> Choice a -> Choice a
->   choice2 = Choice
+    choice2 :: Choice a -> Choice a -> Choice a
+    choice2 = Choice
 
->   choice3 :: Choce a -> Choice a -> Choice a -> Choice a
->   choice3 c1 c2 c3 = Choice c1 (Choice c2 c3)
+    choice3 :: Choce a -> Choice a -> Choice a -> Choice a
+    choice3 c1 c2 c3 = Choice c1 (Choice c2 c3)
 
->   choice4 :: Choice a -> Choice a -> Choice a -> Choice a -> Choice a
->   choice4 c1 c2 c3 c4 = choice2 (choice2 c1 c2) (choice2 c3 c4)
+    choice4 :: Choice a -> Choice a -> Choice a -> Choice a -> Choice a
+    choice4 c1 c2 c3 c4 = choice2 (choice2 c1 c2) (choice2 c3 c4)
 
->   choice5 :: Choice a -> Choice a -> Choice a
->               -> Choice a -> Choice a -> Choice a
->   choice5 c1 c2 c3 c4 c5 =
->       choice2 (choice2 c1 c2) (choice3 c1 c2 c3)
+    choice5 :: Choice a -> Choice a -> Choice a
+                -> Choice a -> Choice a -> Choice a
+    choice5 c1 c2 c3 c4 c5 =
+        choice2 (choice2 c1 c2) (choice3 c1 c2 c3)
 
->   choice6 :: Choice a -> Choice a -> Choice a
->               -> Choice a -> Choice a -> Choice a -> Choice a
->   choice6 c1 c2 c3 c4 c5 c6 =
->       choice3 (choice2 c1 c2) (choice2 c3 c4) (choice2 c5 c6)
+    choice6 :: Choice a -> Choice a -> Choice a
+                -> Choice a -> Choice a -> Choice a -> Choice a
+    choice6 c1 c2 c3 c4 c5 c6 =
+        choice3 (choice2 c1 c2) (choice2 c3 c4) (choice2 c5 c6)
 
->   choice7 :: Choice a -> Choice a -> Choice a -> Choice a
->               -> Choice a -> Choice a -> Choice a -> Choice a
->   choice7 c1 c2 c3 c4 c5 c6 c7 =
->       choice2 (choice3 c1 c2 c3) (choice4 c4 c5 c6 c7)
+    choice7 :: Choice a -> Choice a -> Choice a -> Choice a
+                -> Choice a -> Choice a -> Choice a -> Choice a
+    choice7 c1 c2 c3 c4 c5 c6 c7 =
+        choice2 (choice3 c1 c2 c3) (choice4 c4 c5 c6 c7)
 
->   choices :: Choice a -> [ Choice a ] -> Choice a
->   choices x lst = fini $ foldr inc [ Just x ] lst
->       where
->           -- inc :: Choice a -> [ Maybe (Choice a) ]
->           --        -> [ (Maybe Choice a) ]
->           inc y [] = [ Just y ]
->           inc y ((Just x) : xs) =
->               Nothing : inc (Choice y x) xs
->           inc y (Nothing : xs) = (Just y) : xs
->           -- fini :: [ Maybe (Choice a) ] -> Choice a
->           fini [] = error "Unreachable code reached" -- Not reachable
->           fini (Nothing : xs) = fini xs
->           fini ((Just x) : xs) = fini2 x xs
->           fini2 x [] = x
->           fini2 x (Nothing : xs) = fini2 x xs
->           fini2 x ((Just y) : ys) = fini2 (Choice x y) ys
+    choices :: Choice a -> [ Choice a ] -> Choice a
+    choices x lst = fini $ foldr inc [ Just x ] lst
+        where
+            -- inc :: Choice a -> [ Maybe (Choice a) ]
+            --        -> [ (Maybe Choice a) ]
+            inc y [] = [ Just y ]
+            inc y ((Just x) : xs) =
+                Nothing : inc (Choice y x) xs
+            inc y (Nothing : xs) = (Just y) : xs
+            -- fini :: [ Maybe (Choice a) ] -> Choice a
+            fini [] = error "Unreachable code reached" -- Not reachable
+            fini (Nothing : xs) = fini xs
+            fini ((Just x) : xs) = fini2 x xs
+            fini2 x [] = x
+            fini2 x (Nothing : xs) = fini2 x xs
+            fini2 x ((Just y) : ys) = fini2 (Choice x y) ys
 
->       
+    followedBy :: Choice a -> Choice a -> Choice a
+    followedBy (Choice a b) c =
+                Choice (a `followedBy` c) (b `followedBy` c)
+    followedBy (Emit x a) = Emit x (a `followedBy` c)
+    followedBy Done c = c
 
-\subsection{Functor, Applicative, and Monad }
 
->   instance Functor Choice where
->       fmap f (Choice a b) = Choice (fmap f a) (fmap f b)
->       fmap f (Emit x a) = Emit (f x) (fmap f a)
->       fmap _ Done = Done
+    instance Functor Choice where
+        fmap f (Choice a b) = Choice (fmap f a) (fmap f b)
+        fmap f (Emit x a) = Emit (f x) (fmap f a)
+        fmap _ Done = Done
 
->   followedBy :: Choice a -> Choice a -> Choice a
->   followedBy (Choice a b) c =
->               Choice (a `followedBy` c) (b `followedBy` c)
->   followedBy (Emit x a) = Emit x (a `followedBy` c)
->   followedBy Done c = c
+    bind :: Choice a -> (a -> Choice b) -> Choice b
+    bind (Choice a b) f = Choice (bind a f) (bind b f)
+    bind (Emit x a) f = f x `followedBy` (bind a f)
+    bind Done _ = Done
 
->   bind :: Choice a -> (a -> Choice b) -> Choice b
->   bind (Choice a b) f = Choice (bind a f) (bind b f)
->   bind (Emit x a) f = f x `followedBy` (bind a f)
->   bind Done _ = Done
+    instance Applicative Choice where
+        fs <*> xs = bind fs (\f -> bind xs (\x -> emit (f x)))
 
->   instance Applicative Choice where
->       fs <*> xs = bind fs (\f -> bind xs (\x -> emit (f x)))
-
->   instance Monad Choice where
->       return = emit
->       (>>=) = bind
+    instance Monad Choice where
+        return = emit
+        (>>=) = bind
 
