6b0811e5977c83f44f2ec291d89b99525f16b5c1
Brian Hurt <bhurt42@gmail.com>
2015-09-27 17:31:52 -0400

    Update the Choice data structure.
    Add data on the leaves, rejigger the API.  It still needs comments.

--- src/Choice.hs
+++ src/Choice.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleInstances #-}
 
 {-|
 Module              : Choice
@@ -8,138 +9,86 @@ Maintainer          : bhurt42@gmail.com
 Stability           : experimental
 Portability         : safe
 
-The biggest problem in making a compiler backend is that there are three
-different algorithms: instruction selection, register allocation, and
-instruction scheduling.  By themselves, the algorithms are not of
-overwhelming complexity, The problem is, decisions made by any one of
-the algorithms impact the decisions made by all the rest- and the right
-decision for one algorithm to pick may change depending upon the state
-of the other algorithms.
-
-For example, on the Intel x86 architecture, if we want to add the
-constant @7@ to an expression, we can do this in (at least) two
-different ways.  We can use the @ADD@ instruction, or we can use the
-@LEA@ (Load Effective Address) instruction.  Which of these two
-instructions should the instruction selection algorithm pick?
-
-This depends upon the results of the other two algorithms.  The target
-of the @ADD@ instruction can be in memory, while this is not true
-for the @LEA@- however, the result of the @LEA@ can be stored in
-a different register than the target, which is not true of the @ADD@
-instruction.  So the decisions of the register allocator influence which
-of the two possible instructions is better.
-
-But the instruction scheduler also has implications for which
-instruction should be selected.  The @ADD@ instruction takes an
-arithmetic functional unit, while the @LEA@ uses a load functional
-unit.  Which instruction is optimal to emit depends upon what other
-instructions are being issued at the same time, and thus what functional
-units are free.
-
-And this is a fairly simple example- in reality, things can be
-significantly more complicated, as instructions require values to be in
-specific registers or sets of registers, the register allocator needs to
-figure out which values to spill and fill, and instruction scheduling
-rules become complicated.  The correct decision for any stage to make
-depends upon the results of all the other stages.
-
-The classical solution to this problem is to just implement all three
-algorithms at the same time.  This is why implementing a compiler back
-end is considered such a professional coup.  While each stage is, by
-itself, not of overwhelming the complexity, the combination of all three
-challenge human intellectual capacity.
-
-Unfortunately, in addition to making the result hard to implement, it
-also makes it very hard to understand, change, or tune.  More
-complicated algorithms that might produce better results are often not
-implemented, due simply to the complexity of doing so.  Certainly
-experimentation is reduced due to the high cognitive overhead.
-
-BAM approaches the problem in a radically different way.  Rather than
-trying to figure out the right decision apriori, it simply records that
-a choice needs to be made.  Rather than the instructions being created
-in a simple list, they are produced in a multi-tail list, or choice
-tree.
-
+This is the Choice data structure, the core data structure to BAM.  It
+represents a multi-tailed list or choice tree.
 -}
 module Choice where
 
-    data Choice a =
-        Choice (Choice a) (Choice a)
-        | Emit a (Choice a)
-        | Done
+    import Data.Bifunctor
 
-    toList :: Choice a -> [ [ a ] ]
-    toList (Choice a b) = toList a ++ toList b
-    toList (Emit x a) = fmap (x:) (toList a)
-    toList Done = [ [] ]
+    data Choice a b =
+        Choice (Choice a b) (Choice a b)    -- | Tree-like branch node
+        | Emit b (Choice a b)               -- | List-like sequence node
+        | Done a                            -- | Leaf node
 
-    emit :: a -> Choice a
-    emit x = Emit x Done
+    emit :: a -> b -> Choice a b
+    emit a b = Emit b (Done a)
 
-    choice2 :: Choice a -> Choice a -> Choice a
-    choice2 = Choice
+    fromList :: [ Choice a b ] -> Choice a b
+    fromList [] = error "Empty choices list!"
+    fromList [x] = x
+    fromList (x:xs) = Choice x (fromList xs)
 
-    choice3 :: Choice a -> Choice a -> Choice a -> Choice a
-    choice3 c1 c2 c3 = Choice c1 (Choice c2 c3)
-
-    choice4 :: Choice a -> Choice a -> Choice a -> Choice a -> Choice a
-    choice4 c1 c2 c3 c4 = choice2 (choice2 c1 c2) (choice2 c3 c4)
-
-    choice5 :: Choice a -> Choice a -> Choice a
-                -> Choice a -> Choice a -> Choice a
-    choice5 c1 c2 c3 c4 c5 =
-        choice2 (choice2 c1 c2) (choice3 c3 c4 c5)
-
-    choice6 :: Choice a -> Choice a -> Choice a
-                -> Choice a -> Choice a -> Choice a -> Choice a
-    choice6 c1 c2 c3 c4 c5 c6 =
-        choice3 (choice2 c1 c2) (choice2 c3 c4) (choice2 c5 c6)
-
-    choice7 :: Choice a -> Choice a -> Choice a -> Choice a
-                -> Choice a -> Choice a -> Choice a -> Choice a
-    choice7 c1 c2 c3 c4 c5 c6 c7 =
-        choice2 (choice3 c1 c2 c3) (choice4 c4 c5 c6 c7)
-
-    choices :: Choice a -> [ Choice a ] -> Choice a
-    choices x lst = fini $ foldr inc [ Just x ] lst
+    choices :: Choice a b -> [ Choice a b ]
+    choices = go []
         where
-            -- inc :: Choice a -> [ Maybe (Choice a) ]
-            --        -> [ (Maybe Choice a) ]
-            inc y [] = [ Just y ]
-            inc y ((Just z) : zs) =
-                Nothing : inc (Choice y z) zs
-            inc y (Nothing : zs) = (Just y) : zs
-            -- fini :: [ Maybe (Choice a) ] -> Choice a
-            fini [] = error "Unreachable code reached" -- Not reachable
-            fini (Nothing : zs) = fini zs
-            fini ((Just z) : zs) = fini2 z zs
-            fini2 z [] = z
-            fini2 z (Nothing : ys) = fini2 z ys
-            fini2 z ((Just y) : ys) = fini2 (Choice z y) ys
+            go acc (Choice a b) = go (go acc b) a
+            go acc x = x : acc
 
-    followedBy :: Choice a -> Choice a -> Choice a
-    followedBy (Choice a b) c =
-                Choice (a `followedBy` c) (b `followedBy` c)
-    followedBy (Emit x a) c = Emit x (a `followedBy` c)
-    followedBy Done c = c
+    emits :: Choice a b -> ([b], Choice a b)
+    emits = go []
+        where
+            go acc (Emit x b) = go (x : acc) b
+            go acc t = ((reverse acc), t)
 
+    isDone :: Choice a b -> Bool
+    isDone (Done _) = True
+    isDone _ = False
 
-    instance Functor Choice where
+    extend :: (a -> Choice c b) -> Choice a b -> Choice c b
+    extend f (Choice a b) = Choice (extend f a) (extend f b)
+    extend f (Emit x a) = Emit x (extend f a)
+    extend f (Done s) = f s
+
+    followedBy :: Choice a' b -> Choice a b -> Choice a b
+    followedBy a b = extend (const b) a
+
+    instance Functor (Choice s) where
         fmap f (Choice a b) = Choice (fmap f a) (fmap f b)
         fmap f (Emit x a) = Emit (f x) (fmap f a)
-        fmap _ Done = Done
+        fmap _ (Done s) = Done s
 
-    bind :: Choice a -> (a -> Choice b) -> Choice b
+    instance Bifunctor Choice where
+        bimap f g (Choice a b) = Choice (bimap f g a) (bimap f g b)
+        bimap f g (Emit x a) = Emit (g x) (bimap f g a)
+        bimap f _ (Done s) = Done (f s)
+        first f (Choice a b) = Choice (first f a) (first f b)
+        first f (Emit x a) = Emit x (first f a)
+        first f (Done s) = Done (f s)
+        second g (Choice a b) = Choice (second g a) (second g b)
+        second g (Emit x a) = Emit (g x) (second g a)
+        second _ (Done s) = Done s
+
+    bind :: Choice a b -> (b -> Choice a c) -> Choice a c
     bind (Choice a b) f = Choice (bind a f) (bind b f)
     bind (Emit x a) f = f x `followedBy` (bind a f)
-    bind Done _ = Done
+    bind (Done s) _ = Done s
 
-    instance Applicative Choice where
-        pure = emit
-        fs <*> xs = bind fs (\f -> bind xs (\x -> emit (f x)))
+    instance Applicative (Choice ()) where
+        pure = emit ()
+        fs <*> xs = bind fs (\f -> bind xs (\x -> emit () (f x)))
 
-    instance Monad Choice where
-        return = emit
+    instance Monad (Choice ()) where
+        return = emit ()
         (>>=) = bind
+
+    trunc :: Choice a b -> Choice () b
+    trunc = first $ const ()
+
+    modify :: (t -> a -> Choice t b) -> (s -> t -> u) -> t
+                -> Choice s a -> Choice u b
+    modify f g t (Choice a b) = Choice (modify f g t a) (modify f g t b)
+    modify f g t (Emit x a) = extend (\t' -> modify f g t' a) (f t x)
+    modify _ g t (Done s) = Done (g s t)
+
 
